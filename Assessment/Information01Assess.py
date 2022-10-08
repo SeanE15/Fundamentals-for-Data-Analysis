65a6f6dc0402a112eff91a4bd39ee88de2c62f91
Sean Elliott <G00411288@gmit.ie>
2022-10-08 11:17:09 +0100

    Started stripping para to get clean string of letters

--- Assessment/Information01Assess.py
+++ Assessment/Information01Assess.py
@@ -5,6 +5,7 @@
 # We start by importing the text file from our source using the built-in import function.
 
 import urllib.request
+import random 
 
 # The URL of a text version of Alice in Wonderland.
 url = 'https://www.gutenberg.org/files/11/11-0.txt'
@@ -18,6 +19,15 @@ book = [line.decode('utf-8-sig').strip() for line in book]
 # I then hand pick a paragraph (avoiding the preamble at the start, index, table fo contents etc).
 paragraph = ' '.join(book[85:89])
 
-# Show the paragraph.
-print(paragraph)
+# Define parameters with which we want to form the 1000 character long string.
+chars = 'abcdefghijklmnopqrstuvwxyz '
+
+# Tidy text file by ensuring everythign lowercase
+alice = paragraph.lower() 
+
+# Strip punctuation 
+alice = ''.join([c for c in alice if c in chars])
+
+#test print
+print (alice) 
 
