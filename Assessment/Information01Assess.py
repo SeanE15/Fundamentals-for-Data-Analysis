acc434f8f0f2cea09f5d89b367063d72d0a50763
Sean Elliott <G00411288@gmit.ie>
2022-10-13 20:02:21 +0100

    Edited Information 01 assessment

--- Assessment/Information01Assess.py
+++ Assessment/Information01Assess.py
@@ -31,10 +31,26 @@ alice = ''.join([c for c in alice if c in chars])
 # get length of paragraph
 N = len(alice)
 
-# generate random characters from chars and set strign length to 1000 letters.
-gener = random.choices(chars, k=1000)
+# create below the weights for the 
+weights = [alice.count(c) for c in chars] 
+twoghts = {c: {d: alice.count(c + d) for d in chars} for c in chars} 
 
-#join letters into a string
-gener = ''.join(gener)
+pairs = ' '
 
-print(gener)
+for i in range(1, N):
+    # Get the weights where the previous character is the last character in twos.
+    wt = twoghts[pairs[-2]]
+    # turn wt to a list , ordered by chars 
+    wt =[wt[c] for c in chars]
+    # Randomly pick the next character using those weights.
+    nextc = random.choices(chars, weights=wt, k=1000)[-2]
+    # Append the character to twos.
+    pairs = pairs + nextc
+
+# generate random characters from chars and set string length to 1000 letters.
+# = random.choices(chars, weights=weights, k=1000)
+
+# join letters into a string
+#gener = ''.join(gener)
+
+print(pairs)
